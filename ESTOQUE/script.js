4eb3f4191e6f8e1bd36be22f4865b7c601b88a6d
YandiOrlando <99653816+YandiOrlando@users.noreply.github.com>
2022-04-06 08:32:05 -0300

    Add files via upload

---
+++ ESTOQUE/script.js
@@ -0,0 +1,79 @@
+
+let btnEnviar = document.querySelectorAll('#botoes button')[0];
+let btnExcluir = document.querySelectorAll('#botoes button')[1];
+let btnEditar = document.querySelectorAll('#botoes button')[2];
+let btnVender = document.querySelectorAll('#botoes button')[3];
+let btnPesquisar = document.querySelectorAll('#botoes button')[4];
+
+let nome = document.querySelectorAll('#wrap input')[0];
+let quantidade = document.querySelectorAll('#wrap input')[1];
+let preco = document.querySelectorAll('#wrap input')[2];
+
+let tabela = document.querySelector('#saida table');
+let BD = [];
+
+//métodos
+btnEnviar.onclick = function(){
+    let produto = new Object();
+    produto.nome = nome.value;
+    produto.quantidade = quantidade.value;
+    produto.preco = preco.value;
+    produto.id = BD.length;
+    BD.push(produto);
+    tabela.innerHTML += `<tr><td><input type="checkbox" id="${produto.id}" onchange="verificar(this.id)"></td><td>${produto.nome}</td><td>${produto.quantidade}</td><td>${preco.value}</td></tr>`;
+}
+
+btnExcluir.onclick = function(){
+    for (let i = 0; i < BD.length; i++){
+        let elemento = document.querySelectorAll('#saida table tr td input')[i];
+        if (elemento.checked){
+            BD.splice(elemento.id, 1);
+            tabela.innerHTML = `<tr><td width="30px"></td><td>Nome</td><td>Quant.</td><td>Preço</td></tr>`;
+            montarTabela();
+        }
+    }
+}
+
+function montarTabela(){
+    for (let i = 0; i < BD.length; i++){
+        tabela.innerHTML += `<tr><td width="30px"><input type="checkbox" id="${i}" onchange="verificar()"></td><td>${BD[i].nome}</td><td>${BD[i].quantidade}</td><td>${BD[i].preco}</td></tr>`;
+    }
+}
+
+btnEditar.onclick = function(){
+    for (let i = 0; i < BD.length; i++){
+        let elemento = document.querySelectorAll('#saida table tr td input')[i];
+        if (elemento.checked){
+            BD[i].nome = nome.value;
+            BD[i].quantidade = quantidade.value;
+            BD[i].preco = preco.value;
+
+            tabela.innerHTML = `<tr><td width="30px"></td><td>Nome</td><td>Quant.</td><td>Preço</td></tr>`;
+            montarTabela();
+        }
+    }    
+}
+
+function verificar(id){
+    let cont = 0;
+    for (let i = 0; i < BD.length; i++){
+        let elemento = document.querySelectorAll('#saida table tr td input')[i];
+        if (elemento.checked){
+            nome.value = BD[i].nome;
+            quantidade.value = BD[i].quantidade;
+            preco.value = BD[i].preco;
+            
+            cont++;
+            if (cont > 1){
+                alert('Não é possível selecionar mais de 1 elemento.');
+                elemento.checked = false;
+                break;
+            }
+        }
+    }
+}
+
+btnVender.onclick = function(){
+    
+}
+
